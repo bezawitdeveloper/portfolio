f2a39e38c3df19c8f5bc1162fa94c62f17ece3ec
bezawitdeveloper <62867051+bezawitdeveloper@users.noreply.github.com>
2020-10-05 15:11:35 -0400

    Updates

---
+++ precache-manifest.e6974d0c81a3677d698a5fa48b04bf1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d157ed980c70a7cc7a02725aa86d7dd1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e0c6ff2ddadc1a569275",
+    "url": "/portfolio/static/css/main.b802401d.chunk.css"
+  },
+  {
+    "revision": "20965cd0d3787f71bf7a",
+    "url": "/portfolio/static/js/2.3817dd26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3817dd26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c6ff2ddadc1a569275",
+    "url": "/portfolio/static/js/main.25402f96.chunk.js"
+  },
+  {
+    "revision": "2a0252c53e6f55f84432",
+    "url": "/portfolio/static/js/runtime-main.8611ae6b.js"
+  },
+  {
+    "revision": "23620b2d20ec9bf3d15502f3b2e6031b",
+    "url": "/portfolio/static/media/bg_img1.23620b2d.jpg"
+  }
+]);
